f4ce00fa177d352bbfff4dc0ce69a31477f9827e
Shalinda Ranasinghe <shalinda@gmail.com>
2026-02-18 11:05:31 -0500

    Enhance runscript.sh to include additional SQL scripts
    Added execution of vw_Invoicedetails.sql and storedprocedure.sql during SQL Server setup.

--- sqls/runscript.sh
+++ sqls/runscript.sh
@@ -17,12 +17,20 @@ echo "Starting SQL Server setup for InvoiceDB and SupersetDB..."
 echo "Copying Invoice SQL scripts to container..."
 docker cp sqls/setup-invoice-db.sql $SQLSERVER_CONTAINER:/setup-invoice-db.sql
 docker cp sqls/insert-invoices.sql $SQLSERVER_CONTAINER:/insert-invoices.sql
+docker cp sqls/vw_Invoicedetails.sql $SQLSERVER_CONTAINER:/vw_Invoicedetails.sql
+docker cp sqls/storedprocedure.sql $SQLSERVER_CONTAINER:/storedprocedure.sql
 
 echo "Running setup-invoice-db.sql..."
 MSYS_NO_PATHCONV=1 docker exec -i "$SQLSERVER_CONTAINER" sh -c "/opt/mssql-tools18/bin/sqlcmd -S 127.0.0.1 -U sa -P \"$SA_PASSWORD\" -C -i /setup-invoice-db.sql"
 
 echo "Running insert-invoices.sql..."
 MSYS_NO_PATHCONV=1 docker exec -i "$SQLSERVER_CONTAINER" sh -c "/opt/mssql-tools18/bin/sqlcmd -S 127.0.0.1 -U sa -P \"$SA_PASSWORD\" -C -i /insert-invoices.sql"
+
+echo "Running vw_Invoicedetails.sql..."
+MSYS_NO_PATHCONV=1 docker exec -i "$SQLSERVER_CONTAINER" sh -c "/opt/mssql-tools18/bin/sqlcmd -S 127.0.0.1 -U sa -P \"$SA_PASSWORD\" -C -i /vw_Invoicedetails.sql"
+
+echo "Running storedprocedure.sql..."
+MSYS_NO_PATHCONV=1 docker exec -i "$SQLSERVER_CONTAINER" sh -c "/opt/mssql-tools18/bin/sqlcmd -S 127.0.0.1 -U sa -P \"$SA_PASSWORD\" -C -i /storedprocedure.sql"
 
 # -------------------------------
 # Step 2: Setup Superset Database
